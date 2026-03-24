dd7b79c3e1eadd4595d69997f31cb2b83dcb4814
Marcelo A.B.C. <marcelo.coldibelli@gmail.com>
2026-03-24 09:43:08 -0300

    fix(reconciliation): declare apache/kafka as compatible substitute
    Testcontainers KafkaContainer requires asCompatibleSubstituteFor
    when using a non-default image.

--- reconciliation-service/src/test/java/br/com/codaline/reconciliation/IntegrationTestBase.java
+++ reconciliation-service/src/test/java/br/com/codaline/reconciliation/IntegrationTestBase.java
@@ -22,7 +22,8 @@ public abstract class IntegrationTestBase {
 
   @Container
   static final KafkaContainer kafka =
-      new KafkaContainer(DockerImageName.parse("apache/kafka:3.7.0"));
+      new KafkaContainer(DockerImageName.parse("apache/kafka:3.7.0")
+          .asCompatibleSubstituteFor("confluentinc/cp-kafka"));
 
   @DynamicPropertySource
   static void containerProperties(DynamicPropertyRegistry registry) {
